e0246c91ca8f6023db9ca61de6f76119ad5e5548
agarreta <46403868+agarreta@users.noreply.github.com>
2020-06-02 22:59:05 +0200

    Update arcade_single.py

--- uct/arcade_single.py
+++ uct/arcade_single.py
@@ -17,12 +17,13 @@ from multiprocessing import Pool
 from math import ceil
 import datetime
 plt.interactive(True)
-
+import numpy as np
 
 def solve_pool(args, pool, model_folder, model_filename, mode, seed, num_cpus=1):
     seed_everything(seed)
     if num_cpus == 1:
         results = individual_player_session((args, pool, model_folder, model_filename, mode, seed))
+        results['sat_steps'] = results['sat_steps_taken']
     else:
         p = Pool(num_cpus)
         chunk = ceil(len(pool)/num_cpus)
@@ -41,23 +42,31 @@ def solve_pool(args, pool, model_folder, model_filename, mode, seed, num_cpus=1)
                 results['sat_times'] += sr['sat_times']
                 results['eqs_solved_Z3'] += sr['eqs_solved_Z3']
                 results['sat_steps'] += sr['sat_steps_taken']
-    num_solved = sum([len(x) for x in results['eqs_solved']])
     eqs_solved = set([x[1] for x in results['eqs_solved']])
-    score = len([x[1] for x in results['eqs_solved']])
-    time_avg = sum([x[-1] for x in results['eqs_solved']]) / max(1, score)
+    sc = np.array([x[1] for x in results['eqs_solved']])
+    tm = np.array([x[-1] for x in results['eqs_solved']])
+    score = len(sc)
+    time_avg = np.mean(tm)
+    time_std = np.std(tm)
     l=len(results['sat_steps'])
-    steps_avg = sum(results['sat_steps'])/max(1,l)
+    steps = np.array(results['sat_steps'])
+    steps_avg = np.mean(steps)
+    steps_std = np.std(steps)
     if args.test_solver:
         solver_eqs_solved = set([x[1] for x in results['eqs_solved_Z3']])
         solver_score = len([x[1] for x in results['eqs_solved_Z3']])
-        solver_time_avg = sum([x[-1] for x in results['eqs_solved_Z3']])/max(1,solver_score)
+        solver_tm = np.array([x[-1] for x in results['eqs_solved_Z3']])
+        solver_time_avg = np.mean(solver_tm)
+        solver_time_std = np.std(solver_tm)
 
         intersection = eqs_solved & solver_eqs_solved
-        intersection_solved = [x for x in results['eqs_solved'] if x[1] in intersection]
-        intersection_solver_solved = [x for x in results['eqs_solved_Z3'] if x[1] in intersection]
+        intersection_solved = np.array([x[-1] for x in results['eqs_solved'] if x[1] in intersection])
+        intersection_solver_solved = np.array([x[-1] for x in results['eqs_solved_Z3'] if x[1] in intersection])
 
-        intersection_times = sum([x[-1] for x in intersection_solved])/max(1,len(intersection_solved))
-        intersection_times_solver = sum([x[-1] for x in intersection_solver_solved])/max(1,len(intersection_solver_solved))
+        intersection_times = np.mean(intersection_solved)
+        intersection_times_std = np.std(intersection_solved)
+        intersection_times_solver =np.mean(intersection_solver_solved)
+        intersection_times_solver_std =np.std(intersection_solver_solved)
     else:
         solver_time_avg = -1
         solver_score = -1
@@ -66,8 +75,9 @@ def solve_pool(args, pool, model_folder, model_filename, mode, seed, num_cpus=1)
 
     log = f'\nPool {args.pool_name}, Seed: {args.seed_class}\n' \
           f'Model: {os.path.join(model_folder, model_filename)}\n' \
-          f'Score: {score}, Time avg: {time_avg}, Intersection time avg: {intersection_times}, Num steps avg: {steps_avg} ({l})\n' \
-          f'Solver score: {solver_score}, Solver time avg: {solver_time_avg}, Intersection time avg: {intersection_times_solver},\n' \
+          f'Score: {score}, Time avg: {time_avg} ({time_std}), Intersection time avg: {intersection_times} ({intersection_times_std}), Num steps avg: {steps_avg} ({steps_std}) ({l})\n' \
+          f'Solver score: {solver_score}, Solver time avg: {solver_time_avg} ({solver_time_std}), Intersection time avg: {intersection_times_solver} ({intersection_times_solver_std}),\n' \
+          f'Steps: {steps}\n'\
           f'SIDE_MAX_LEN: {args.SIDE_MAX_LEN}, num_vars: {len(args.VARIABLES)}, num_letters: {len(args.ALPHABET)}\n' \
           f'num_channels: {args.num_channels}, num_residual_blocks: {args.num_resnet_blocks}\n' \
           f'num mcts simulations: {args.num_mcts_simulations}\n' \
@@ -78,8 +88,30 @@ def solve_pool(args, pool, model_folder, model_filename, mode, seed, num_cpus=1)
     with open(f'log_{args.smt_solver}.txt', 'a+') as f:
         f.write(log)
 
+    log_full = f'\nPool {args.pool_name}, Seed: {args.seed_class}\n' \
+          f'Model: {os.path.join(model_folder, model_filename)}\n' \
+          f'Score: {score}, Time avg: {time_avg} ({time_std}), Intersection time avg: {intersection_times} ({intersection_times_std}), Num steps avg: {steps_avg} ({steps_std}) ({l})\n' \
+          f'Solver score: {solver_score}, Solver time avg: {solver_time_avg} ({solver_time_std}), Intersection time avg: {intersection_times_solver} ({intersection_times_solver_std}),\n' \
+          f'Solved: {eqs_solved}\n' \
+          f'Solver solved: {solver_eqs_solved}\n' \
+          f'Intersection: {intersection}\n'\
+          f'Times: {tm}\n' \
+          f'Solver times: {solver_tm}\n' \
+          f'Intersection times: {intersection_solved}\n' \
+          f'Intersection solver times: {intersection_solver_solved}\n' \
+          f'SIDE_MAX_LEN: {args.SIDE_MAX_LEN}, num_vars: {len(args.VARIABLES)}, num_letters: {len(args.ALPHABET)}\n' \
+          f'num_channels: {args.num_channels}, num_residual_blocks: {args.num_resnet_blocks}\n' \
+          f'num mcts simulations: {args.num_mcts_simulations}\n' \
+          f'Max solver time in mcts: {args.mcts_smt_time_max}\n' \
+          f'Comments: {args.log_comments}\n' \
+          f'Date: {datetime.datetime.today()}\n'
+
+    with open(f'log_full_{args.smt_solver}.txt', 'a+') as f:
+        f.write(log_full)
+
 def individual_player_session(play_args):
     args, pool, model_folder, model_filename, mode, seed = play_args
+    seed_everything(seed)
     results = dict({'level': args.level})
 
     nnet = NNetWrapper(args, device='cpu', training=False, seed=seed)
